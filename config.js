cc8ad850f48f2a1f783f5941e8ac232e46ad8026
Newton Jain <newton.jain@gmail.com>
2017-06-24 20:09:47 -0700

    Can create one note pages

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //Live Connect API information
+    clientId: '21ec55e0-4e3a-4a11-a260-94b0cd17a7da',
+    clientSecret: 'uopyGLFZyhP4QB0AmAhAJm6 ',
+    redirectUrl: 'http://localhost:3000/callback'
+};
